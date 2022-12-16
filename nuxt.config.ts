fb948639f56e7f9c59b5e7d917d03cd58f9824ad
fazz <nurfaizi.faza@gmail.com>
2022-12-16 10:40:23 +0700

    chore(lint): setup Eslint and Prettier

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
+import eslintPlugin from 'vite-plugin-eslint';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+	vite: {
+		plugins: [eslintPlugin()],
+	},
+	modules: ['@nuxtjs/tailwindcss'],
+});
